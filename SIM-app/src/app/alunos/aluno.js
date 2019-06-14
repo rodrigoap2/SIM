22e3c1d25658ccc75de9c323b602c0a0825f33d7
rodrigoap2 <rap2@cin.ufpe.br>
2019-06-14 18:32:53 -0300

    [cdbm#4]Implementação de testes de classe para o cadastro de alunos

--- SIM-app/src/app/alunos/aluno.js
+++ SIM-app/src/app/alunos/aluno.js
@@ -1,41 +1,41 @@
 "use strict";
-exports.__esModule = true;
-var Aluno = /** @class */ (function () {
-    function Aluno() {
+Object.defineProperty(exports, "__esModule", { value: true });
+class Aluno {
+    constructor() {
         this.clean();
     }
-    Aluno.prototype.clean = function () {
+    clean() {
         this.nome = "";
         this.login = "";
         this.criterios = new Map();
-    };
-    Aluno.prototype.clone = function () {
+    }
+    clone() {
         var aluno = new Aluno();
         aluno.criterios = new Map();
         aluno.copyFrom(this);
         return aluno;
-    };
-    Aluno.prototype.copyFrom = function (from) {
+    }
+    copyFrom(from) {
         this.nome = from.nome;
         this.login = from.login;
         this.copyCriteriosFrom(from.criterios);
-    };
-    Aluno.prototype.copyCriteriosFrom = function (from) {
+    }
+    copyCriteriosFrom(from) {
         this.criterios = new Map();
-        for (var key in from) {
+        for (let key in from) {
             this.criterios[key] = from[key];
         }
-    };
-    Aluno.prototype.verificaAluno = function () {
+    }
+    verificaAluno() {
         var conceitos = [];
-        for (var key in this.criterios) {
+        for (let key in this.criterios) {
             if (this.criterios[key] <= 0 || this.criterios[key] > 10) {
                 this.criterios[key] = '';
                 conceitos.push(key);
             }
         }
         return conceitos;
-    };
-    return Aluno;
-}());
+    }
+}
 exports.Aluno = Aluno;
+//# sourceMappingURL=aluno.js.map
